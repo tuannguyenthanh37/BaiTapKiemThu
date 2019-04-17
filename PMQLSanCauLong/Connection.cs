3ee31fafc0a660fc0750c8ef69a3cdbdacea2dda
tuannguyenthanh37 <48054586+tuannguyenthanh37@users.noreply.github.com>
2019-04-17 16:04:49 +0700

    Tạo class connect
    Tạo class connect

---
+++ PMQLSanCauLong/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PMQLSanCauLong
+{
+    public class Connection
+    {
+        public SqlConnection cnn;
+        string connectionStr = @"server=desktop-jcsiium;database=DBSanCauLong;integrated security=true";
+        public Connection()
+        {
+            cnn = new SqlConnection(connectionStr);
+        }
+        public Connection(string s)
+        {
+            cnn = new SqlConnection(s);
+        }
+        // hàm mở kết nối
+        public void KetNoi()
+        {
+            if (cnn.State == ConnectionState.Closed)
+                cnn.Open();
+        }
+        // Hàm đóng kết nối
+        public void DongKetNoi()
+        {
+            if (cnn.State == ConnectionState.Open)
+                cnn.Close();
+        }
+    }
+}
